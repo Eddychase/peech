325bd8f92edc12c17b16f01a38240b52449e8f47
Tom Chege <emdeechege@gmail.com>
2018-09-07 15:04:16 +0300

    login okay..

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
 #...
@@ -8,7 +9,7 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     author = db.Column(db.String(255))
@@ -16,8 +17,20 @@ class User(db.Model):
     password_hash = db.Column(db.String(255))
     pitches_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
     def __repr__(self):
-        return f'User {self.username}'
+        return f'User {self.author}'
 
 class Pitches(db.Model):
     __tablename__= 'pitches'
