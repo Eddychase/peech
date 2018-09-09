6459b7c2ee189f5c67dc3d3255acf2d30d893f28
Tom Chege <emdeechege@gmail.com>
2018-09-09 13:39:05 +0300

    trouble deplying

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
 
     SECRET_KEY=os.environ.get('SECRET_KEY')
 
-    # SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://emdee:arif@123@localhost/pitches'
+
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
@@ -29,7 +29,7 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    
+
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://emdee:arif@123@localhost/pitches_test'
 
