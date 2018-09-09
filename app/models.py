2e79668e59c7be75ba902060bac3a3d1d244c5fb
Tom Chege <emdeechege@gmail.com>
2018-09-09 20:50:56 +0300

    sorted views py

--- app/models.py
+++ app/models.py
@@ -33,20 +33,8 @@ class User(UserMixin,db.Model):
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
 
-    @property
-    def password(self):
-        raise AttributeError('You cannnot read the password attribute')
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-
-    def verify_password(self,password):
-        return check_password_hash(self.password_hash,password)
-
     def __repr__(self):
-        return f'User {self.author}'
+        return f' {self.author}'
 
 class Pitches(db.Model):
     __tablename__= 'pitches'
