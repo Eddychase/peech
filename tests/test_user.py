a564d8c36c57deb830789c35acddb2db24a7206b
Tom Chege <emdeechege@gmail.com>
2018-09-11 12:06:33 +0300

    revisit tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import User
+from app.models import User,Comments, Pitches
 
 class UserModelTest(unittest.TestCase):
 
@@ -7,19 +7,25 @@ class UserModelTest(unittest.TestCase):
         self.new_user = User(password = 'banana')
 
     def test_password_setter(self):
-<<<<<<< HEAD
-        self.assertTrue(self.new_user.pass_secure is not None)
-=======
         self.assertTrue(self.new_user.password_hash is not None)
->>>>>>> dev
 
     def test_no_access_password(self):
-            with self.assertRaises(AttributeError):
-                self.new_user.password
+        with self.assertRaises(AttributeError):
+            self.new_user.password
 
-<<<<<<< HEAD
-        def test_password_verification(self):
-=======
     def test_password_verification(self):
->>>>>>> dev
-            self.assertTrue(self.new_user.verify_password('banana'))
+        self.assertTrue(self.new_user.verify_password('banana'))
+
+class CommentsUserModelTest(unittest.TestCase):
+
+    def setUp(self):
+       self.new_comment = Comments(user_id = 2, comment = 'cross buns',pitches_id = '5',date_posted='2018-09-09')
+
+    def test_save_comment(self):
+
+        self.assertTrue(len(Comments.query.all())>0)
+
+class PitchesModelTest(unittest.TestCase):
+
+    def test_save_pitch(self):
+        self.assertTrue(len(Comments.query.all())>0)
