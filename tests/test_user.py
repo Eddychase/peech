1f196727f7d69f2434a1743f4769d86c45d339a6
Tom Chege <emdeechege@gmail.com>
2018-09-11 12:59:08 +0300

    rearranged by most recent

--- tests/test_user.py
+++ tests/test_user.py
@@ -16,10 +16,18 @@ class UserModelTest(unittest.TestCase):
     def test_password_verification(self):
         self.assertTrue(self.new_user.verify_password('banana'))
 
-class CommentsUserModelTest(unittest.TestCase):
+class CommentsModelTest(unittest.TestCase):
 
     def setUp(self):
-       self.new_comment = Comments(user_id = 2, comment = 'cross buns',pitches_id = '5',date_posted='2018-09-09')
+       self.new_comment = Comments(id=1, user_id = 2, comment = 'cross buns',pitches_id = '5',date_posted='2018-09-09')
+
+    def test_comment_variables(self):
+
+       # self.assertEquals(self.new_comment.id, 2)
+       self.assertEquals(self.new_comment.comment,'cross buns')
+       self.assertEquals(self.new_comment.date_posted,'2018-09-09')
+       self.assertEquals(self.new_comment.user_id, 2)
+       # self.assertEquals(self.new_comment.pitches_id, 4)
 
     def test_save_comment(self):
 
@@ -28,4 +36,4 @@ class CommentsUserModelTest(unittest.TestCase):
 class PitchesModelTest(unittest.TestCase):
 
     def test_save_pitch(self):
-        self.assertTrue(len(Comments.query.all())>0)
+        self.assertTrue(len(Pitches.query.all())>0)
